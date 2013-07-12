d971151852756df301a4c95d20fbbc52046565e2
Matthew O'Brien <matthew.obrien@guardian.co.uk>
2013-07-12 16:04:28 +0100

    Using new stats API

---
+++ src/test/scala/FastlyAPIClientTest.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mobrien
+ * Date: 12/07/2013
+ * Time: 13:19
+ * To change this template use File | Settings | File Templates.
+ */
+class FastlyAPIClientTest {
+
+}
---
+++ src/test/scala/FastlyCredentials.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mobrien
+ * Date: 12/07/2013
+ * Time: 14:02
+ * To change this template use File | Settings | File Templates.
+ */
+trait FastlyCredentials {
+
+}
